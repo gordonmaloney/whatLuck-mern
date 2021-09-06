2ca83371be5153ea42488cb65269fee78dd9b5a3
Gordon Maloney <gordonmaloney@gmail.com>
2021-09-06 15:32:17 +0100

    delete reducer

--- client/src/reducers/potlucks.js
+++ client/src/reducers/potlucks.js
@@ -2,12 +2,18 @@ export default (potlucks = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
       return action.payload;
+
     case "CREATE":
       return [...potlucks, action.payload];
+
     case "UPDATE":
       return potlucks.map((potluck) =>
       potluck._id === action.payload._id ? action.payload : potluck
       );
+
+    case 'DELETE':
+      return potlucks.filter((potluck) => potluck._id !== action.payload);
+      
     default:
       return potlucks;
   }
