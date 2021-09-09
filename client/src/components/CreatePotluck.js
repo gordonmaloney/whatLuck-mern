294f27205d26981558c6dd2cfef8c1a3d0211c23
Gordon Maloney <gordonmaloney@gmail.com>
2021-09-09 15:54:33 +0100

    functionality complete

--- client/src/components/CreatePotluck.js
+++ client/src/components/CreatePotluck.js
@@ -14,6 +14,7 @@ import { createPotluck } from "../actions/potlucks";
 import randomWords from "random-words";
 import { Card, CardHeader, CardBody } from "reactstrap";
 
+
 const CreatePotluck = () => {
   const potlucks = useSelector((state) => state.potlucks);
 
--- client/src/components/Examples.js
+++
@@ -1,1 +0,0 @@
-const Examples = [];
